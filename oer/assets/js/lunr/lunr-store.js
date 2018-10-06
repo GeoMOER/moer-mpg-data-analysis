6a641ceb6bc46434272076139d5c0930e609be0e
tnauss <nauss@staff.uni-marburg.de>
2018-10-06 20:30:15 +0200

    Add unit 01 and adjust for OER

---
+++ oer/assets/js/lunr/lunr-store.js
@@ -0,0 +1,61 @@
+var store = [{
+        "title": "First things first",
+        "excerpt":"In this session we make a brute force introduction to R, R markdown, the RStudio IDE, version management with Git and GitHub’s classroom functionality. While - from a learning theory point of view - this does not really represent anything but a worst case example, these are the things you...","categories": ["Unit 01"],
+        "tags": ["Unit|01"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit01/unit01-01.html",
+        "teaser":null},{
+        "title": "Git and GitHub",
+        "excerpt":"To start with a clarification: Git is the version control system you will use in this course (and which is used by millions of other developers.) With Git you will manage something called repository which is the place where you manage your software project (i.e. a folder on your hard...","categories": ["Unit 01"],
+        "tags": ["Unit|01"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit01/unit01-02.html",
+        "teaser":null},{
+        "title": "Assignment: Hello R, hello GitHub",
+        "excerpt":"This worksheet introduces you to R, R scripts and R markdown. Your submission will be pushed to your class repository at GitHub. After completion you should have gained some experience in running R commands within scripts, R markdown for running code and documentation within one file and Git/GitHub. Things you...","categories": ["Unit 01"],
+        "tags": ["Unit|01"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit01/unit01-03.html",
+        "teaser":null},{
+        "title": "Standing on the shoulders of (messy) giants",
+        "excerpt":"Building things from scratch is time consuming, error prone and - thanks to many people distributing their work with open licenses - generally not necessary. Open science is not just a public relation term but strives for a community which works together on a subject and openly shares data, methods...","categories": ["Unit 02"],
+        "tags": ["Unit|02"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit02/unit02-01.html",
+        "teaser":null},{
+        "title": "PA: R Package",
+        "excerpt":"One R package, one script collection and two GitHub repositories Using the readily available scripts as a basis we will create an R package for the more general functionality and an R script collection (e.g. R Studio project or just a folder with scripts) for the more specific tasks. Each...","categories": ["Unit 02"],
+        "tags": ["Unit|02"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit02/unit02-02.html",
+        "teaser":null},{
+        "title": "PR: Function review and bug fixing",
+        "excerpt":"Implementing an R Package is and keeping track of all things is quite a headache. Hence, we will cross check the functions following a peer review approach. Peer review assignment Please review the functions of another peer from the beechForestDynamics R package. If the functions need some enhancement or bug...","categories": ["Unit 02"],
+        "tags": ["Unit|02"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit02/unit02-03.html",
+        "teaser":null},{
+        "title": "PA: Automatic script",
+        "excerpt":"Automatic processing of MODIS data using the R package functions Now that a first reviewed version of the beechForestDynamics is at hand, use the functionality to implement a script which handles the MODIS processing automatically following the template provided in project_beech_dynamics and already used as part of Unit02 | PA:...","categories": ["Unit 02"],
+        "tags": ["Unit|02"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit02/unit02-04.html",
+        "teaser":null},{
+        "title": "Overviews and in-depth analysis",
+        "excerpt":"You already came up with some guiding questions as part of DP: Questions and work packages. They focus on potential differences between productivity dynamics of primary (Beech) and other forests and the influence/buffering of climatic fluctuations by primary forests. Let’s put this and the processed MODIS data resulting from Unit02...","categories": ["Unit 03"],
+        "tags": ["Unit|03"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit03/unit03-01.html",
+        "teaser":null},{
+        "title": "DP: Analysis questions",
+        "excerpt":"Given the target area of Romania and keeping the role of spatial forest disturbances in mind, it is time to fine tune the guiding questions for the analysis. Group discussion prompt Please refine your guiding questions from DP: Questions and work packages and focus on the study region of Romania....","categories": ["Unit 03"],
+        "tags": ["Unit|03"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit03/unit03-02.html",
+        "teaser":null},{
+        "title": "AA: Forest dynamics",
+        "excerpt":"Analysis assignment Please form groups of two and and choose one of the final questions from DP: Analysis questions as your guiding frame. Compile the necessary analysis. Scripts Please create a team folder within the readily existing GitHub repository project_beech_dynamics and add your analysis scripts into this folder. Keep to...","categories": ["Unit 03"],
+        "tags": ["Unit|03"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit03/unit03-03.html",
+        "teaser":null},{
+        "title": "NA: Communicate your findings",
+        "excerpt":"After finishing your analysis, it is time to communicate your results. Short note assignment Please write a short note on your analysis question, methods, results and critical discussion of your findings. Structure of your short note Have a look at Detsch et al. 2016 for structuring your short note. Given...","categories": ["Unit 04"],
+        "tags": ["Unit|04"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit04/unit04-01.html",
+        "teaser":null},{
+        "title": "DP: What have I done",
+        "excerpt":"We reached the end of this module. It is time to reflect your work. Group discussion prompt Please reflect your work and discuss your impressions in the group following the guiding questions below. ","categories": ["Unit 05"],
+        "tags": ["Unit|05"],
+        "url": "https://oer.uni-marburg.de/data/mriliasmooc/lm_data/lm_2777/unit05/unit05-01.html",
+        "teaser":null},]
